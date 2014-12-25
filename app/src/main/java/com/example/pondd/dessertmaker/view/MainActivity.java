1afd64ac187428a4b79a2dc7e14381bb788d8f42
ountzza <pond1982a@gmail.com>
2014-12-25 16:47:11 +0700

    complete actionbar drawer

--- app/src/main/java/com/example/pondd/dessertmaker/view/MainActivity.java
+++ app/src/main/java/com/example/pondd/dessertmaker/view/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.pondd.dessertmaker.view;
 
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,12 +13,15 @@ import com.example.pondd.dessertmaker.fragment.FragmentMain;
 
 
 public class MainActivity extends ActionBarActivity {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initInstance();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentContainer, FragmentMain.newInstance(), "FragmentMain")
@@ -23,6 +29,28 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    public void initInstance() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,
+                drawerLayout,
+                R.string.hello_world,
+                R.string.hello_world);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,6 +61,8 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
